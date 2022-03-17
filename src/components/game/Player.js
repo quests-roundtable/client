bc6b835f5158e13070eaeb945ccd88c87c10d792
Malcolm Yeh <malcolmyeh@gmail.com>
2022-03-17 16:17:38 -0400

    visible amour/ally

--- src/components/game/Player.js
+++ src/components/game/Player.js
@@ -3,7 +3,7 @@ import Card from "./Card";
 import { QUEST, TOURNAMENT, SPONSOR, ROUND_END } from "../../util/constants";
 
 
-function Player({state, playerNum, player, style, roundType, result}) {
+function Player({ state, playerNum, player, style, roundType, result }) {
 
     function getStyle() {
         var style = {
@@ -13,36 +13,44 @@ function Player({state, playerNum, player, style, roundType, result}) {
         return style;
     }
 
-    const moveInfo = roundType == QUEST ? player.questInfo : 
+    const moveInfo = roundType == QUEST ? player.questInfo :
         roundType == TOURNAMENT ? player.tournamentInfo : null
 
-    const event = roundType == QUEST ? state.quest : 
+    const event = roundType == QUEST ? state.quest :
         roundType == TOURNAMENT ? state.tournament : null
-
-    return(
+    console.log("num special cards:", player.specialCards.length)
+    console.log("showing ", moveInfo?.numMoveCards - player.specialCards.length, " cards")
+    return (
         <div className={`player${playerNum} grid-a`} style={style}>
-            <div className= {`rank${playerNum} grid-a`} style={{
+            <div className={`rank${playerNum} grid-a`} style={{
                 borderColor: "darkslategray", borderStyle: (moveInfo ? "solid" : "none")
             }}>
                 <Card card={player.rankCard} style={getStyle()} className="card-static" />
             </div>
             <div className={playerNum <= 2 ? "playerMoveH" : "playerMoveV"}>
-                {moveInfo?.role === SPONSOR ? <b>SPONSOR</b> : <></> }
-                {moveInfo?.playerMove ?  
-                    (event.roundStatus == ROUND_END ? 
-                    result?.hand.map((card, idx) => {
-                        return(
-                            <Card key={idx} card={card} style={getStyle()}/>
+                {moveInfo?.role === SPONSOR ? <b>SPONSOR</b> : <></>}
+                <>
+                    {!event || event?.roundStatus !== ROUND_END ? player?.specialCards.map((card, idx) => {
+                        return (
+                            <Card key={idx} card={card} style={getStyle()} />
                         )
-                    })
-                    : (roundType === TOURNAMENT ? 
-                    (moveInfo?.numMoveCards > 0 ? <Card card={{ typeId: "adventure" }} style={getStyle()}/> : <></>)
-                    : [...Array(moveInfo?.numMoveCards)].map((x, i) => {
-                        return(
-                            <Card key={i} card={{ typeId: "adventure" }} style={getStyle()}/>
-                        )
-                    })))
-                : <></>
+                    }) : <></>}
+                </>
+                {moveInfo?.playerMove ?
+                    (event.roundStatus == ROUND_END ?
+                        result?.hand.map((card, idx) => {
+                            return (
+                                <Card key={idx} card={card} style={getStyle()} />
+                            )
+                        })
+                        : (roundType === TOURNAMENT ?
+                            (moveInfo?.numMoveCards > 0 ? <Card card={{ typeId: "adventure" }} style={getStyle()} /> : <></>)
+                            : [...Array(moveInfo?.numMoveCards - player.specialCards.length) ].map((x, i) => {
+                                return (
+                                    <Card key={i} card={{ typeId: "adventure" }} style={getStyle()} />
+                                )
+                            })))
+                    : <></>
                 }
             </div>
         </div>
